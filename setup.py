df8e0e43e7af479f1aa53eae1622b2caf0e79989
mwsluis <msluis@gmail.com>
2017-04-09 08:03:10 +0200

    update version and description.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='nad_receiver',
-      version='0.0.3',
-      description='Library to interface with NAD receivers through the RS232',
+      version='0.0.4',
+      description='Library to interface with NAD receivers through the RS232 and tcp/ip',
       url='https://github.com/joopert/nad_receiver',
       author='joopert',
       author_email=':(',
