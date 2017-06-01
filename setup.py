b435fdfc61ca7d42b8f485b15cab23c9f6833372
joopster <ruben@vhulst.com>
2017-06-01 13:38:54 +0200

    try pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='nad_receiver',
-      version='0.0.4',
-      description='Library to interface with NAD receivers through the RS232',
+      version='0.0.5',
+      description='Library to interface with NAD receivers through RS232 and TCP',
       url='https://github.com/joopert/nad_receiver',
+      download_url='https://github.com/joopert/nad_receiver/archive/0.0.5.tar.gz', #
       author='joopert',
-      author_email=':(',
       license='MIT',
       packages=['nad_receiver'],
       install_requires=['pyserial==3.2.1'],
