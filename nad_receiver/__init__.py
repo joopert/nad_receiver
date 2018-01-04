25d640d733a9a3be96c403004c64d7418e089e40
robhuls <robhuls@gmail.com>
2018-01-04 09:45:54 +0100

    Fix string comparison for source

--- nad_receiver/__init__.py
+++ nad_receiver/__init__.py
@@ -251,7 +251,7 @@ class NADReceiverTCP(object):
         """Select a source from the list of sources."""
         status = self.status()
         if status['power']:  # Changing source when off may hang NAD7050
-            if status['source'] not in source:  # Setting the source to the current source will hang the NAD7050
+            if status['source'] != source:  # Setting the source to the current source will hang the NAD7050
                 if source in self.SOURCES:
                     self._send(self.CMD_SOURCE + self.SOURCES[source], read_reply=True)
 
