4882c543885079f85fc2ddeed67e7685bf06b592
joopert <joopert@users.noreply.github.com>
2020-04-29 17:26:29 +0200

    0.1.0

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='nad_receiver',
-      version='0.0.12',
+      version='0.1.0',
       description='Library to interface with NAD receivers through RS232, TCP and telnet',
       url='https://github.com/joopert/nad_receiver',
-      download_url='https://github.com/joopert/nad_receiver/archive/0.0.12.tar.gz', #
+      download_url='https://github.com/joopert/nad_receiver/archive/0.1.0.tar.gz', #
       author='joopert',
       license='MIT',
       packages=['nad_receiver'],
