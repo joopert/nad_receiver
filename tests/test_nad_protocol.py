0de5394c9f4df3b728502cf73b4e3b41969f3728
Frederik Gladhorn <gladhorn@kde.org>
2020-04-29 17:23:33 +0200

    Add fake NAD C356BE
    This implements the complete RS232 API of the receiver in a mock class.
    With this the receiver can be tested locally without having any serial port.
    
    Add a test that checks that communication with that particular amplifier
    doesn't break again. Similar tests are needed for other models, but
    since this is the only amp I have, I'm not easily able to write them.
    
    The sources are for now left out because the current implementation
    assumes sources to be integers, while the V2 protocol is purely string
    based.

---
+++ tests/test_nad_protocol.py
@@ -0,0 +1,90 @@
+import re
+import pytest  # type: ignore
+
+import nad_receiver
+from nad_receiver.nad_fake_transport import Fake_NAD_C_356BE_Transport
+
+ON = "On"
+OFF = "Off"
+
+
+class Fake_NAD_C_356BE(nad_receiver.NADReceiver):
+    """NAD Receiver with fake transport for testing."""
+    def __init__(self):
+        self.transport = Fake_NAD_C_356BE_Transport()
+
+
+def test_NAD_C_356BE():
+    # This test can be run with the real amplifier, just instantiate
+    # the real transport instead of the fake one
+    receiver = Fake_NAD_C_356BE()
+    assert receiver.main_power("?") in (ON, OFF)
+
+    # switch off
+    assert receiver.main_power("=", OFF) == OFF
+    assert receiver.main_power("?") == OFF
+    assert receiver.main_power("+") == ON
+    assert receiver.main_power("+") == OFF
+    assert receiver.main_power("?") == OFF
+
+    # C 356BE does not reply for commands other than power when off
+    assert receiver.main_mute("?") is None
+
+    assert receiver.main_power("=", ON) == ON
+    assert receiver.main_power("?") == ON
+
+    assert receiver.main_mute("=", OFF) == OFF
+    assert receiver.main_mute("?") == OFF
+
+    # Not a feature for this amp
+    assert receiver.main_dimmer("?") is None
+
+    # Stepper motor and this thing has no idea about the volume
+    assert receiver.main_volume("?") is None
+
+    # No exception
+    assert receiver.main_volume("+") is None
+    assert receiver.main_volume("-") is None
+
+    assert receiver.main_version("?") == "V1.02"
+    assert receiver.main_model("?") == "C356BEE"
+
+    # Here the RS232 NAD manual seems to be slightly off / maybe the model is different
+    # The manual claims:
+    # CD Tuner Video Disc Ipod Tape2 Aux
+    # My Amp:
+    # CD Tuner Disc/MDC Aux Tape2 MP
+    ### FIXME: Protocol V2 represents sources as strings, we should get these:
+    # assert receiver.main_source("=", "AUX") == "AUX"
+    # assert receiver.main_source("?") == "AUX"
+    # assert receiver.main_source("=", "CD") == "CD"
+    # assert receiver.main_source("?") == "CD"
+    # assert receiver.main_source("+") == "TUNER"
+    # assert receiver.main_source("-") == "CD"
+    # assert receiver.main_source("+") == "TUNER"
+    # assert receiver.main_source("+") == "DISC/MDC"
+    # assert receiver.main_source("+") == "AUX"
+    # assert receiver.main_source("+") == "TAPE2"
+    # assert receiver.main_source("+") == "MP"
+    # assert receiver.main_source("+") == "CD"
+    # assert receiver.main_source("-") == "MP"
+
+    # Tape monitor / tape 1 is independent of sources
+    assert receiver.main_tape_monitor("=", OFF) == OFF
+    assert receiver.main_tape_monitor("?") == OFF
+    assert receiver.main_tape_monitor("=", ON) == ON
+    assert receiver.main_tape_monitor("+") == OFF
+
+    assert receiver.main_speaker_a("=", OFF) == OFF
+    assert receiver.main_speaker_a("?") == OFF
+    assert receiver.main_speaker_a("=", ON) == ON
+    assert receiver.main_speaker_a("?") == ON
+    assert receiver.main_speaker_a("+") == OFF
+    assert receiver.main_speaker_a("+") == ON
+    assert receiver.main_speaker_a("-") == OFF
+    assert receiver.main_speaker_a("-") == ON
+
+    assert receiver.main_speaker_b("=", OFF) == OFF
+    assert receiver.main_speaker_b("?") == OFF
+
+    assert receiver.main_power("=", OFF) == OFF
