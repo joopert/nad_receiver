e9d790aacd1fa2a2fc94933700b8762ac4cb65d7
Frederik Gladhorn <gladhorn@kde.org>
2020-04-29 18:05:01 +0200

    Dynamic API
    We have a complete description of the protocol in nad_commands. Let's
    use it. This dynamically generates objects from the command dictionary,
    so that new functions do not need any code any more. Instead one can
    simply call:
    receiver.main.volume.increase()
    
    This works by inspecting nad_commands when asking for attributes of the
    receiver class or it's dynamically created subclasses.
    
    The old API is around unchanged to stay compatible.

--- tests/test_nad_protocol.py
+++ tests/test_nad_protocol.py
@@ -14,7 +14,7 @@ class Fake_NAD_C_356BE(nad_receiver.NADReceiver):
         self.transport = Fake_NAD_C_356BE_Transport()
 
 
-def test_NAD_C_356BE():
+def test_NAD_C_356BE_old_api():
     # This test can be run with the real amplifier, just instantiate
     # the real transport instead of the fake one
     receiver = Fake_NAD_C_356BE()
@@ -88,3 +88,120 @@ def test_NAD_C_356BE():
     assert receiver.main_speaker_b("?") == OFF
 
     assert receiver.main_power("=", OFF) == OFF
+
+
+def test_NAD_C_356BE_new_api():
+    # This test can be run with the real amplifier, just instantiate
+    # the real transport instead of the fake one
+    receiver = Fake_NAD_C_356BE()
+    assert receiver.main.power.get() in (ON, OFF)
+
+    # switch off
+    assert receiver.main.power.set(OFF) == OFF
+    assert receiver.main.power.get() == OFF
+    assert receiver.main.power.increase() == ON
+    assert receiver.main.power.increase() == OFF
+    assert receiver.main.power.get() == OFF
+
+    # C 356BE does not reply for commands other than power when off
+    with pytest.raises(ValueError):
+        receiver.main.mute.get()
+
+    assert receiver.main.power.set(ON) == ON
+    assert receiver.main.power.get() == ON
+
+    assert receiver.main.mute.set(OFF) == OFF
+    assert receiver.main.mute.get() == OFF
+
+    # Not a feature for this amp
+    with pytest.raises(ValueError):
+        receiver.main.dimmer.get()
+
+    # Stepper motor and this thing has no idea about the volume
+    with pytest.raises(ValueError):
+        receiver.main.volume.get()
+
+    # No exception
+    assert receiver.main.volume.increase() is None
+    assert receiver.main.volume.decrease() is None
+
+    assert receiver.main.version.get() == "V1.02"
+    assert receiver.main.model.get() == "C356BEE"
+
+    # Here the RS232 NAD manual seems to be slightly off / maybe the model is different
+    # The manual claims:
+    # CD Tuner Video Disc Ipod Tape2 Aux
+    # My Amp:
+    # CD Tuner Disc/MDC Aux Tape2 MP
+    assert receiver.main.source.set("AUX") == "AUX"
+    assert receiver.main.source.get() == "AUX"
+    assert receiver.main.source.set("CD") == "CD"
+    assert receiver.main.source.get() == "CD"
+    assert receiver.main.source.increase() == "TUNER"
+    assert receiver.main.source.decrease() == "CD"
+    assert receiver.main.source.increase() == "TUNER"
+    assert receiver.main.source.increase() == "DISC/MDC"
+    assert receiver.main.source.increase() == "AUX"
+    assert receiver.main.source.increase() == "TAPE2"
+    assert receiver.main.source.increase() == "MP"
+    assert receiver.main.source.increase() == "CD"
+    assert receiver.main.source.decrease() == "MP"
+
+    # Tape monitor / tape 1 is independent of sources
+    assert receiver.main.tape_monitor.set(OFF) == OFF
+    assert receiver.main.tape_monitor.get() == OFF
+    assert receiver.main.tape_monitor.set(ON) == ON
+    assert receiver.main.tape_monitor.increase() == OFF
+
+    assert receiver.main.speaker_a.set(OFF) == OFF
+    assert receiver.main.speaker_a.get() == OFF
+    assert receiver.main.speaker_a.set(ON) == ON
+    assert receiver.main.speaker_a.get() == ON
+    assert receiver.main.speaker_a.increase() == OFF
+    assert receiver.main.speaker_a.increase() == ON
+    assert receiver.main.speaker_a.decrease() == OFF
+    assert receiver.main.speaker_a.decrease() == ON
+
+    assert receiver.main.speaker_b.set(OFF) == OFF
+    assert receiver.main.speaker_b.get() == OFF
+
+    assert receiver.main.power.set(OFF) == OFF
+
+
+def test_dynamic_api():
+    receiver = Fake_NAD_C_356BE()
+    assert receiver.main.power.get() in (ON, OFF)
+
+    # invalid attributes result in attribute error
+    with pytest.raises(AttributeError):
+        receiver.foo
+    with pytest.raises(AttributeError):
+        receiver.foo.bar
+
+    # valid attributes work and have a good __repr__
+    assert str(receiver.main) == "NADReceiver.main"
+    assert str(receiver.main.power) == "NADReceiver.main.power"
+    assert str(receiver.main.power.get) == "NADReceiver.main.power.get"
+    assert str(receiver.main.power.increase) == "NADReceiver.main.power.increase"
+    assert str(receiver.main.power.decrease) == "NADReceiver.main.power.decrease"
+    assert str(receiver.main.power.set) == "NADReceiver.main.power.set"
+
+    # functions on dynamic objects can be called
+    assert callable(receiver.main.power.get)
+    assert callable(receiver.main.power.set)
+    assert callable(receiver.main.power.increase)
+    assert callable(receiver.main.power.decrease)
+
+    # attributes can not be called
+    with pytest.raises(TypeError, match="object is not callable"):
+        receiver.main()
+    with pytest.raises(TypeError, match="object is not callable"):
+        receiver.main.power()
+
+    # invalid properties are AttributeErrors
+    with pytest.raises(AttributeError):
+        receiver.main.power.invalid
+    with pytest.raises(AttributeError):
+        receiver.main.power.invalid()
+    with pytest.raises(AttributeError):
+        receiver.main.power.get.invalid_too
